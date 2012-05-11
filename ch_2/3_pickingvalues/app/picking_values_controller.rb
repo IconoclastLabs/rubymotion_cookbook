f47e72e5347205e2ea2b42ccc98c1147afc02f05
Matt Garrison <mattsgarrison@gmail.com>
2012-05-11 13:07:26 -0500

    Added empty files to resources so those dirs are added and people cloning will be able to run it out of the box

--- ch_2/3_pickingvalues/app/picking_values_controller.rb
+++ ch_2/3_pickingvalues/app/picking_values_controller.rb
@@ -2,8 +2,8 @@ class PickingValuesController < UIViewController
   def viewDidLoad
     view.backgroundColor = UIColor.whiteColor
     @myPicker = UIPickerView.alloc.init
+    @myPicker.dataSource = self
     @myPicker.center = view.center
-
     view.addSubview(@myPicker)
   end
 end
