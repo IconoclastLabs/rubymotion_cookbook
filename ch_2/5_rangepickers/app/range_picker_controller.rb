b62dacf513bcbbe1eeb9489bcef081ceb9162bca
Matt Garrison <mattsgarrison@gmail.com>
2012-05-11 15:45:19 -0500

    Added a slider that populates a label

---
+++ ch_2/5_rangepickers/app/range_picker_controller.rb
@@ -0,0 +1,22 @@
+class RangePickerController < UIViewController
+  def viewDidLoad
+    view.backgroundColor = UIColor.whiteColor
+    @mySlider = UISlider.alloc.initWithFrame([[0.0,0.0],[200.0,23.0]])
+    @mySlider.center = view.center
+    @mySlider.minimumValue = 0.0
+    @mySlider.maximumValue = 100.0
+    @mySlider.value = @mySlider.maximumValue / 2.0
+    @mySlider.continuous = :no
+    @mySlider.addTarget(self, action:'sliderValueChanged', forControlEvents:UIControlEventValueChanged)
+    view.addSubview(@mySlider)
+    
+    @label = UILabel.new
+    @label.text = 'Slider Value:'
+    @label.frame = [[0,0],[250,150]]
+    view.addSubview(@label)
+  end
+
+  def sliderValueChanged
+    @label.text = "Slider Value: #{@mySlider.value}"
+  end
+end
