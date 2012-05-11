6006fde1405adef5893ec0fdfba74d255bc1f309
Matt Garrison <mattsgarrison@gmail.com>
2012-05-11 15:25:31 -0500

    Added Date selection action

--- ch_2/4_PickingDateTime/app/picking_date_time_controller.rb
+++ ch_2/4_PickingDateTime/app/picking_date_time_controller.rb
@@ -3,19 +3,18 @@ class PickingDateTimeController < UIViewController
     view.backgroundColor = UIColor.whiteColor
     @myDatePicker = UIDatePicker.alloc.init
     @myDatePicker.center = view.center
-    # @myDatePicker.addTarget(self, action:'datePickerChanged', forControlEvents:UIControlEventValueChanged)
-     @myDatePicker.datePickerMode = UIDatePickerDateAndTime
+    @myDatePicker.addTarget(self, action:'datePickerChanged', forControlEvents:UIControlEventValueChanged)
+    @myDatePicker.datePickerMode = UIDatePickerModeDateAndTime
     view.addSubview(@myDatePicker)
 
 
-    #@label = UILabel.new
-    #@label.text = 'Picker Results'
-    #@label.frame = [[0,0],[50,50]]
-    #view.addSubview(@label)
+    @label = UILabel.new
+    @label.text = 'Picker Results'
+    @label.frame = [[0,0],[250,150]]
+    view.addSubview(@label)
   end
 
-#  def datePickerChanged
-#    date = @myDatePicker.date
-#    @label.text = date
-#  end
+  def datePickerChanged
+    @label.text = "Event: #{@myDatePicker.date}"
+  end
 end
