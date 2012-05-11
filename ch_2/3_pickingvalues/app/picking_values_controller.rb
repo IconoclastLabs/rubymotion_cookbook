0ff4b4d03e0349c6fac99f3f45dc878fd237e65d
Matt Garrison <mattsgarrison@gmail.com>
2012-05-11 15:05:45 -0500

    datepicker shows up on the center of the screen

--- ch_2/3_pickingvalues/app/picking_values_controller.rb
+++ ch_2/3_pickingvalues/app/picking_values_controller.rb
@@ -1,9 +1,19 @@
 class PickingValuesController < UIViewController
   def viewDidLoad
     view.backgroundColor = UIColor.whiteColor
-    @myPicker = UIPickerView.alloc.init
+    @myPicker = UIPickerView.alloc.init titleForRow: 1
     @myPicker.dataSource = self
     @myPicker.center = view.center
+
     view.addSubview(@myPicker)
   end
+
+  def pickerView
+    if pickerView.equal = @myPicker
+      result = "Row #{row+1}"
+    end
+    result
+  end
+
+
 end
