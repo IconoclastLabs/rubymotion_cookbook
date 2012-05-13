b6abf97927355446bfcc7264941b820014e1d6fe
Gant <GantMan@gmail.com>
2012-05-12 19:09:54 -0500

    Handling the specific picker text and showing indicator

--- ch_2/3_pickingvalues/app/picking_values_controller.rb
+++ ch_2/3_pickingvalues/app/picking_values_controller.rb
@@ -2,19 +2,26 @@ class PickingValuesController < UIViewController
   def viewDidLoad
     self.view.backgroundColor = UIColor.whiteColor
     @myPicker = UIPickerView.alloc.init
+    @myPicker.showsSelectionIndicator = true
     @myPicker.dataSource = self
+    @myPicker.delegate = self
     @myPicker.center = self.view.center
     @myPicker.center = view.center
 
     self.view.addSubview(@myPicker)
   end
 
+  # Necssary to have these method in order to be a datasource
   def numberOfComponentsInPickerView pickerView
     1 
   end
 
   def pickerView(pickerView, numberOfRowsInComponent:component)
     10
+  end
+
+  def pickerView(pickerView, titleForRow:row, forComponent:component)
+    "I can count to #{row+1}"
   end
 
 end
