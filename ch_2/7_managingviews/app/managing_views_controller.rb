fe2dd710f48fdd5556e1b860dbdbff49f53e981c
Matt Garrison <mattsgarrison@gmail.com>
2012-05-12 22:57:46 -0500

    loaded xib view

--- ch_2/7_managingviews/app/managing_views_controller.rb
+++ ch_2/7_managingviews/app/managing_views_controller.rb
@@ -1,6 +1,6 @@
 class ManagingViewsController < UIViewController
   def viewDidLoad
-    @label = self.view.subviews.first
+    @label = self.view.select('mylabel')
     @label.text = 'Controller Output'
   end
 end
