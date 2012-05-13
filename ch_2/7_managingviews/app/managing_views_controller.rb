7d043c331604dd0e68520fc89709e984b114e42d
Matt Garrison <mattsgarrison@gmail.com>
2012-05-12 23:01:46 -0500

    updated to use XIB tag numbers to access the label

--- ch_2/7_managingviews/app/managing_views_controller.rb
+++ ch_2/7_managingviews/app/managing_views_controller.rb
@@ -1,6 +1,6 @@
 class ManagingViewsController < UIViewController
   def viewDidLoad
-    @label = self.view.select('mylabel')
+    @label = self.view.viewWithTag(1)
     @label.text = 'Controller Output'
   end
 end
