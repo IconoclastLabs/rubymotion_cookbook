5454761dcc68a2aff37eda208525057d3e92c3e0
Matt Garrison <mattsgarrison@gmail.com>
2012-05-12 22:02:52 -0500

    added grouping options

--- ch_2/6_groupingoptions/app/grouping_options_controller.rb
+++ ch_2/6_groupingoptions/app/grouping_options_controller.rb
@@ -3,7 +3,19 @@ class GroupingOptionsController < UIViewController
     view.backgroundColor = UIColor.whiteColor
     options = ["iPhone","iPod","iPad","iMac"]
     @mySegmentedControl = UISegmentedControl.alloc.initWithItems(options)
+    @mySegmentedControl.addTarget(self, action:'segmentChanged', forControlEvents:UIControlEventValueChanged)
     @mySegmentedControl.center = view.center
     view.addSubview(@mySegmentedControl)
+
+    @label = UILabel.new
+    @label.text = 'Picker Results'
+    @label.frame = [[0,0],[250,150]]
+    view.addSubview(@label)
   end
+
+  def segmentChanged(sender)
+    @label.text = sender.selectedSegmentIndex
+  end
+
+
 end
