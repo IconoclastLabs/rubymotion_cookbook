ea7dc2d7ca2c4d85d09ba2f3c305c4100a644769
Matt Garrison <mattsgarrison@gmail.com>
2012-05-13 14:45:06 -0500

    should probably add a button to allow navigation back and forth, but it works

--- ch_2/8_implementingnavigation/app/second_view_controller.rb
+++ ch_2/8_implementingnavigation/app/second_view_controller.rb
@@ -2,5 +2,11 @@ class SecondViewController < UIViewController
   def viewDidLoad
     self.view.backgroundColor = UIColor.greenColor
     self.title = 'Secondary'
+
+    #self.performSelector('goBack', nil, afterDelay:5.0)
+  end
+
+  def goBack
+    self.navigationController.popViewControllerAnimated('yes')
   end
 end
