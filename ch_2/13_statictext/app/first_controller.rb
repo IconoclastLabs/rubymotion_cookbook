ccc4d2c3ddc816d7306e91137ba52ea7b566d2cf
Matt Garrison <mattsgarrison@gmail.com>
2012-05-15 20:58:45 -0500

    text works

---
+++ ch_2/13_statictext/app/first_controller.rb
@@ -0,0 +1,13 @@
+class FirstController < UIViewController
+  def viewDidLoad
+    view.backgroundColor = UIColor.whiteColor
+    @label = UILabel.new
+    @label.text = 'Static Text! asdfsjkasdfjklsd;flhasd;fas;dlkfas;ldfjkalsdkfuhsadfuhasdlfnasdlkfhlasdflsadfjkjdfsjjjpoqwiero,mnzxcv,mnvnoibagfopifq9023487174i'
+    @label.font = UIFont.boldSystemFontOfSize(14)
+    @label.frame = [[50,50],[250,50]]
+    @label.numberOfLines = 3
+    @label.adjustsFontSizeToFitWidth = 'YES'
+    @label.center = view.center
+    view.addSubview(@label)
+  end
+end
