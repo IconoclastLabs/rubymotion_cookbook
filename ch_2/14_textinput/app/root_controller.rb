16d2916a847549e1c5748be5371c89535290f263
Matt Garrison <mattsgarrison@gmail.com>
2012-05-15 21:32:22 -0500

    text input added to view

--- ch_2/14_textinput/app/root_controller.rb
+++ ch_2/14_textinput/app/root_controller.rb
@@ -1,14 +1,18 @@
 class RootController < UIViewController
   def viewDidLoad
     view.backgroundColor = UIColor.whiteColor
-    @label = UILabel.new
-    @label.text = 'Static Text!'
-    @label.font = UIFont.boldSystemFontOfSize(14)
-    @label.frame = [[50,50],[250,50]]
-    @label.numberOfLines = 3
-    @label.adjustsFontSizeToFitWidth = 'YES'
-    @label.center = view.center
-    view.addSubview(@label)
+    @char_pluralization = "Characters"
+    @charcount = UITextField.alloc.initWithFrame([[0,0],[100,31]])
+    
+    @charcount.borderStyle = UITextBorderStyleRoundedRect
+    @charcount.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter
+    @charcount.textAlignment = UITextAlignmentCenter
+    @charcount.text = "0 Characters"
+    @charcount.center = view.center
+    view.addSubview(@charcount)
+
+    @la
+
 
     @text_field = UITextField.alloc.initWithFrame([[0,0],[200,31]])
     @text_field.borderStyle = UITextBorderStyleRoundedRect
@@ -17,5 +21,13 @@ class RootController < UIViewController
     @text_field.text = "The Brave Sir Robin"
     @text_field.center = view.center
     view.addSubview(@text_field)
+  end
+
+  def char_counter(param_string)
+    if param_string.length = 1
+      @char_pluralization = "Character"
+    end
+
+    @charcount
   end
 end
