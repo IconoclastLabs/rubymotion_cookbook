46a72a19ca3d358b640943f1374cd899eda64bd1
Matt Garrison <mattsgarrison@gmail.com>
2012-05-15 20:45:05 -0500

    fixed a loading error of view controllers that made tab images not show

--- ch_2/12_tabbars/app/app_delegate.rb
+++ ch_2/12_tabbars/app/app_delegate.rb
@@ -5,13 +5,8 @@ class AppDelegate
 
     first_tab = FirstTabController.alloc.init
     second_tab = SecondTabController.alloc.init
-    #first_nav = UINavigationController.alloc.initWithRootViewController(first_tab)
-    #second_nav = UINavigationController.alloc.initWithRootViewController(second_tab)
     @tabbar = UITabBarController.alloc.init
     @tabbar.viewControllers = [first_tab, second_tab] 
-
-    #@window.addSubview(tabbar.view)
-    #navbar = UINavigationController.alloc.initWithRootViewController(tabbar.view)
     
     @tabbar.wantsFullScreenLayout = true
     @window.addSubview @tabbar.view
