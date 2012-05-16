99201b6ec9f5bd90da4c7f4fb762ec7727af8eaf
Matt Garrison <mattsgarrison@gmail.com>
2012-05-15 22:43:36 -0500

    bleh.  delegates are annoying

--- ch_2/14_textinput/app/root_controller.rb
+++ ch_2/14_textinput/app/root_controller.rb
@@ -1,33 +1,44 @@
 class RootController < UIViewController
   def viewDidLoad
     view.backgroundColor = UIColor.whiteColor
+
+    @my_text_field = UITextField.alloc.initWithFrame([[0,0],[100,31]])
+    @my_text_field.borderStyle = UITextBorderStyleRoundedRect
+    @my_text_field.textAlignment = UITextAlignmentLeft
+    @my_text_field.text = "Enter text..."
+    @my_text_field.delegate = self
+    view.addSubview(@my_text_field)
+
     @char_pluralization = "Characters"
-    @charcount = UITextField.alloc.initWithFrame([[0,0],[100,31]])
-    
-    @charcount.borderStyle = UITextBorderStyleRoundedRect
-    @charcount.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter
+    @charcount = UITextField.alloc.initWithFrame([[0,0],[200,31]])
+    @charcount.borderStyle = UITextBorderStyleBezel
     @charcount.textAlignment = UITextAlignmentCenter
     @charcount.text = "0 Characters"
     @charcount.center = view.center
     view.addSubview(@charcount)
+    self.calculateAndDisplayTextFieldLengthWithText(@my_text_field.text)
 
-    @la
-
-
-    @text_field = UITextField.alloc.initWithFrame([[0,0],[200,31]])
-    @text_field.borderStyle = UITextBorderStyleRoundedRect
-    @text_field.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter
-    @text_field.textAlignment = UITextAlignmentCenter
-    @text_field.text = "The Brave Sir Robin"
-    @text_field.center = view.center
-    view.addSubview(@text_field)
   end
 
-  def char_counter(param_string)
-    if param_string.length = 1
-      @char_pluralization = "Character"
-    end
 
-    @charcount
+  def textField(shouldChangeCharactersInRange, replacementString:string)
+    result = 'YES'
+    if textField = @my_text_field
+      wholeText = textField.text stringReplacingCharactersInRange(range, withString:string)
+      self.calculateAndDisplayTextFieldLengthWithText.wholeText
+    end
+    result
+  end
+
+  def textFieldShouldReturn textField
+    textField.resignFirstResponder
+   'YES'
+  end
+
+
+
+  def calculateAndDisplayTextFieldLengthWithText(param_string)
+    @char_pluralization = "Character" if param_string.size == 1
+    @char_count.text = "#{param_string} #{@char_pluralization}"
   end
 end
