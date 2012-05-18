8289cc48ffa2692175031079ad39a2f937c78a48
Gant <GantMan@gmail.com>
2012-05-18 11:20:47 -0500

    Got it working, and followed the book a little more in the layout

--- ch_2/14_textinput/app/root_controller.rb
+++ ch_2/14_textinput/app/root_controller.rb
@@ -1,31 +1,33 @@
 class RootController < UIViewController
   def viewDidLoad
+    super
     view.backgroundColor = UIColor.whiteColor
 
-    @my_text_field = UITextField.alloc.initWithFrame([[0,0],[100,31]])
+    @my_text_field = UITextField.alloc.initWithFrame([[38.0,30.0],[220.0,31.0]])
     @my_text_field.borderStyle = UITextBorderStyleRoundedRect
     @my_text_field.textAlignment = UITextAlignmentLeft
     @my_text_field.text = "Enter text..."
     @my_text_field.delegate = self
     view.addSubview(@my_text_field)
 
-    @char_pluralization = "Characters"
-    @charcount = UITextField.alloc.initWithFrame([[0,0],[200,31]])
+    labelCounterFrame = @my_text_field.frame
+    labelCounterFrame.origin.y += labelCounterFrame.size.height + 10
+    @charcount = UITextField.alloc.initWithFrame(labelCounterFrame)
     @charcount.borderStyle = UITextBorderStyleBezel
     @charcount.textAlignment = UITextAlignmentCenter
     @charcount.text = "0 Characters"
-    @charcount.center = view.center
     view.addSubview(@charcount)
     self.calculateAndDisplayTextFieldLengthWithText(@my_text_field.text)
-
   end
 
-
-  def textField(shouldChangeCharactersInRange, replacementString:string)
+  def textField(textField, shouldChangeCharactersInRange:range, replacementString:string)
     result = 'YES'
-    if textField = @my_text_field
-      wholeText = textField.text stringReplacingCharactersInRange(range, withString:string)
-      self.calculateAndDisplayTextFieldLengthWithText.wholeText
+    if textField.isEqual(@my_text_field)
+      # IMPORTANT NOTE:  You can't just use @my_text_field in your function here because this delegate is called
+      # BEFORE the value is placed in the UITextField.  Therefore using @my_text_field would always give you
+      # the size of the field on last edit/paste, and be forever behind by 1 event.
+      wholeText = textField.text.stringByReplacingCharactersInRange(range, withString:string)
+      self.calculateAndDisplayTextFieldLengthWithText(wholeText)
     end
     result
   end
@@ -35,10 +37,8 @@ class RootController < UIViewController
    'YES'
   end
 
-
-
-  def calculateAndDisplayTextFieldLengthWithText(param_string)
-    @char_pluralization = "Character" if param_string.size == 1
-    @char_count.text = "#{param_string} #{@char_pluralization}"
+  def calculateAndDisplayTextFieldLengthWithText(paramText)
+    char_pluralization = (paramText.length == 1) ? ("Character") : ("Characters")
+    @charcount.text = "#{paramText.length} #{char_pluralization}"
   end
 end
