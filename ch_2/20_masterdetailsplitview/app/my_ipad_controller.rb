cd442babb8de46458d90febecc6ab8027135eddd
Matt Garrison <mattsgarrison@gmail.com>
2012-05-22 22:45:12 -0500

    init 23

--- ch_2/20_masterdetailsplitview/app/my_ipad_controller.rb
+++ ch_2/20_masterdetailsplitview/app/my_ipad_controller.rb
@@ -1,5 +1,9 @@
 class MyIpadController < UIViewController
   def viewDidLoad
+    @my_table_view = UITableView.alloc.initWithFrame(view.bounds,style:UITableViewStylePlain)
+    @my_table_view.dataSource = 
+    view.addSubview(@my_table_view)
+    @my_detail_view = DetailViewController.alloc.init
     @label = UILabel.alloc.init
     @label.text = "Only iPads see this"
     @label.frame = [[0,0],[250,150]]
