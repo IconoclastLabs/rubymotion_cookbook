dfaff46c5785049c51b0ff4ce6da1f8784e1a52f
Matt Garrison <mattsgarrison@gmail.com>
2012-06-06 22:50:16 -0500

    working through the rotation handler

---
+++ ch_7/02_rotationgestures/app/root_controller.rb
@@ -0,0 +1,33 @@
+class RootController < UIViewController
+  def viewDidLoad
+    view.backgroundColor = UIColor.whiteColor
+
+    @label = UILabel.alloc.initWithFrame(CGRectZero)
+    @label.text = "Hello World"
+    @label.font = UIFont.systemFontOfSize(16)
+    @label.sizeToFit
+    @label.center = view.center
+    view.addSubview(@label)
+
+    @rotation_gesture_recognizer = UIRotationGestureRecognizer.alloc.initWithTarget(self, action:"handle_rotations:")
+    view.addGestureRecognizer(@rotation_gesture_recognizer)
+  end
+
+  def viewDidUnload
+    @rotation_gesture_recognizer = nil
+    @label = nil
+  end
+
+  def handle_rotations(sender)
+    if(@label == nil)
+      return
+    end
+
+    @label.transform = CGAffineTransformMakeRotation(rotationAngleInRadians + sender.rotation)
+
+    if(sender.state == UIGestureRecognizerStateEnded)
+      self.rotationAngleInRadians += sender.rotation
+    end
+
+  end
+end
