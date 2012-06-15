6ce0d1831004d044f543d7b964a84795071cbc1c
Gant <GantMan@gmail.com>
2012-06-14 22:00:27 -0500

    Added book delay and more

--- ch_2/08_implementingnavigation/app/impl_nav_controller.rb
+++ ch_2/08_implementingnavigation/app/impl_nav_controller.rb
@@ -8,10 +8,17 @@ class ImplNavController < UIViewController
     @label.frame = [[50,50],[250,50]]
     view.addSubview(@label)
    
+    # Push after 5 second (this is what the book did)
+    self.performSelector('push_second_controller:', withObject:nil, afterDelay: 5)
+
+    #allow user to force it... why wait right?
+    self.navigationItem.rightBarButtonItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemAdd, target:self, action: 'push_second_controller:')
+    
+  end
+
+  def push_second_controller(param)
     @secondViewController = SecondViewController.alloc.init
     self.navigationController.pushViewController(@secondViewController, animated:'YES')
-    #self.pushViewController = @secondViewController
-    
   end
 
 end
