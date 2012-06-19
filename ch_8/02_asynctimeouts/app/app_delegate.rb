d312ce1964566170676aaabd23650c010ca22675
Matt Garrison <mattsgarrison@gmail.com>
2012-06-18 23:39:14 -0500

    Works when async succeeds, crashed with '> terminate called without an active exception%' when networking is disabled

---
+++ ch_8/02_asynctimeouts/app/app_delegate.rb
@@ -0,0 +1,37 @@
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.rootViewController = RootController.new
+
+    url = NSURL.URLWithString("http://www.apple.com")
+    request = NSURLRequest.requestWithURL(url,
+                                          cachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData,
+                                          timeoutInterval:30)
+    queue = NSOperationQueue.alloc.init
+
+    NSURLConnection.sendAsynchronousRequest(request,
+      queue: queue,
+      completionHandler: lambda do |response, data, error|
+        if(data.length > 0 && error.nil?)
+          doc_dir = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
+                                                        NSUserDomainMask,
+                                                        'YES').objectAtIndex(0)
+          #p doc_dir
+          file_path = doc_dir.stringByAppendingPathComponent("apple.html")
+
+          data.writeToFile(file_path, atomically: 'YES')
+
+          p "Saved file to #{file_path}"
+        elsif( data.length == 0 && error.nil? )
+          p "Nothing was downloaded"
+        elsif(error?)
+          p "Error: #{error}"
+        end
+      end)
+
+
+    @window.makeKeyAndVisible
+    true
+  end
+end
+
