42b01d3e92afbd96318a7bf55f615967aaa6d354
Gant <GantMan@gmail.com>
2012-06-27 12:40:34 -0500

    Got iPhone working, need to debug ipad issue

--- ch_2/22_displayingpopovers/app/app_delegate.rb
+++ ch_2/22_displayingpopovers/app/app_delegate.rb
@@ -1,8 +1,9 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(RootController.alloc.init)
+    @window.rootViewController.wantsFullScreenLayout = true
     @window.makeKeyAndVisible
-    @window.rootViewController = RootViewController.new
     true
   end
 end
