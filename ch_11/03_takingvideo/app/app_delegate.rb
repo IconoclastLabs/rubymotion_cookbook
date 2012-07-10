97b383184498d407b597b973623266eb84c15fd4
Gant <GantMan@gmail.com>
2012-07-09 20:52:20 -0500

    Fixed to use navigation controller, as designed

--- ch_11/03_takingvideo/app/app_delegate.rb
+++ ch_11/03_takingvideo/app/app_delegate.rb
@@ -1,7 +1,8 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.rootViewController = RootController.alloc.init
+    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(RootController.alloc.init)
+    @window.rootViewController.wantsFullScreenLayout = true
     @window.makeKeyAndVisible
     true
   end
