f9ff6bce6927ad2778ac838a41f4f7ffb029704e
Matt Garrison <mattsgarrison@iconoclastlabs.com>
2012-07-15 13:33:15 -0500

    added an image to post to the tweet

--- ch_8/11_twitterintegration/app/root_controller.rb
+++ ch_8/11_twitterintegration/app/root_controller.rb
@@ -2,12 +2,16 @@ class RootController < UIViewController
   def viewDidLoad
     view.backgroundColor = UIColor.whiteColor
     self.title = "Twitter Integration"
+    # Word of advice: You'll need to sign into Twitter in the simulator for this to fully
+    # function and this *will* actually post to that account.
     @twitter_controller = TWTweetComposeViewController.new
-    @twitter_controller.setInitialText("Sample tweet from RubyMotion")
+    @twitter_controller.addImage(UIImage.imageNamed("smilingbob.png"))
+    @twitter_controller.addURL(NSURL.URLWithString("http://www.rubymotion.com/developer-center/"))
+    @twitter_controller.setInitialText("RubyMotion is awesome.")
     self.navigationController.presentModalViewController(@twitter_controller, animated:true)
   end
 
   def viewDidUnload
-
+    @twitter_controller = nil
   end
 end
