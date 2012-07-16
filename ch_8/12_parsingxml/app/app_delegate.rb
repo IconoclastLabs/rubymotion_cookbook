131970322e8b62719fae01c08f9eeffbeece05f4
Matt Garrison <mattsgarrison@iconoclastlabs.com>
2012-07-16 12:41:49 -0500

    added cocoapod RaptureXML to complete this chapter because screw NSXMLParser's tag soup

--- ch_8/12_parsingxml/app/app_delegate.rb
+++ ch_8/12_parsingxml/app/app_delegate.rb
@@ -1,5 +1,9 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.rootViewController = RootController.new
+
+    @window.makeKeyAndVisible
     true
   end
 end
