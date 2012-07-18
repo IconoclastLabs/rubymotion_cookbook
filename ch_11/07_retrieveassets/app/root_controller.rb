094c685d4514585d3bc7ba572a1903fe8d881d2c
Gant <GantMan@gmail.com>
2012-07-18 08:38:49 -0500

    Awaiting info from Rubymotion on these

--- ch_11/07_retrieveassets/app/root_controller.rb
+++ ch_11/07_retrieveassets/app/root_controller.rb
@@ -6,15 +6,15 @@ class RootController < UIViewController
     @assets_library = ALAssetsLibrary.alloc.init
 
     @assets_library.enumerateGroupsWithTypes(ALAssetsGroupAll, usingBlock:lambda do |group, stop|
-      group.enumerateAssetsUsingBlock(lambda do |result, index, stop|
-        assetType = result.valueForProperty(ALAssetPropertyType)
+      #group.enumerateAssetsUsingBlock(lambda do |result, index, stop|
+      #  assetType = result.valueForProperty(ALAssetPropertyType)
 
-        p "This is a photo" if assetType == ALAssetTypePhoto
-        p "This is a video" if assetType == ALAssetTypeVideo
-        p "This is uknown" if assetType == ALAssetTypeUnknown
+      #  p "This is a photo" if assetType == ALAssetTypePhoto
+      #  p "This is a video" if assetType == ALAssetTypeVideo
+      #  p "This is uknown" if assetType == ALAssetTypeUnknown
 
 
-        end)
+      #  end)
       end,
       failureBlock:lambda do |error|
         p "Failed to enumerate the asset groups"
