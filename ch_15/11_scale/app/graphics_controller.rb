b0b093b0cdd6d26b5fc2ae54a11332e6b2f1a82a
Gant <GantMan@gmail.com>
2012-07-24 22:29:53 -0500

    Completed 15.11

---
+++ ch_15/11_scale/app/graphics_controller.rb
@@ -0,0 +1,44 @@
+class GraphicsController < UIView
+  def initWithFrame(frame)
+    super
+
+    self.backgroundColor = UIColor.lightGrayColor
+    self
+  end
+
+  def drawRect(rect)
+
+    path = CGPathCreateMutable()
+
+    rectangle1 = CGRectMake(10, 10, 200, 300)
+
+    CGPathAddRect(path, nil, rectangle1)
+
+    currentContext = UIGraphicsGetCurrentContext()
+    
+    CGContextSaveGState(currentContext)
+
+    CGContextScaleCTM(currentContext, 0.5, 0.5)
+
+    CGContextAddPath(currentContext, path)
+
+    UIColor.colorWithRed(0.2, green:0.6, blue:0.8, alpha:1).setFill
+
+    UIColor.blackColor.setStroke
+
+    CGContextSetLineWidth(currentContext, 5)
+
+    CGContextDrawPath(currentContext, KCGPathFillStroke)
+
+    #release path object is not necessary and will actually cause a crash!  as of this writing.
+    #retain, release and autorelease messages are not necessary in rubymotion
+    # http://www.rubymotion.com/developer-center/guides/runtime/
+    #
+    #CGPathRelease(path)
+
+    CGContextRestoreGState(currentContext)
+
+  end
+
+
+end
