3826e8dbc115c284c1c6b11b38685738bef1c906
Gant <GantMan@gmail.com>
2013-04-27 15:36:48 -0500

    Fixing early misconception on yes/no true/false conversions

--- ch_2/19_webview/app/root_controller.rb
+++ ch_2/19_webview/app/root_controller.rb
@@ -5,7 +5,7 @@ class RootController < UIViewController
     view.backgroundColor = UIColor.whiteColor
     @my_web_view = UIWebView.alloc.initWithFrame(view.bounds)
     @my_web_view.delegate = self
-    @my_web_view.scalesPageToFit = 'YES'
+    @my_web_view.scalesPageToFit = false
 
     view.addSubview(@my_web_view)
 
@@ -16,14 +16,14 @@ class RootController < UIViewController
   end
 
   def webViewDidStartLoad(webView)
-    UIApplication.sharedApplication.setNetworkActivityIndicatorVisible('YES')
+    UIApplication.sharedApplication.setNetworkActivityIndicatorVisible(true)
   end
 
   def webViewDidFinishLoad(webView)
-    UIApplication.sharedApplication.setNetworkActivityIndicatorVisible('NO')
+    UIApplication.sharedApplication.setNetworkActivityIndicatorVisible(false)
   end
 
   def webView(webView, didFailLoadWithError:error)
-    UIApplication.sharedApplication.setNetworkActivityIndicatorVisible('NO')
+    UIApplication.sharedApplication.setNetworkActivityIndicatorVisible(false)
   end
 end
