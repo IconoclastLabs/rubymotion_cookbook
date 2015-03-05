db0bef9256740a9fe29734f32d2a46e217f77552
Frank von Hoven <fvonhoven@gmail.com>
2015-03-05 15:19:13 -0600

    Added cust splash screens

--- ch_2/rp_alerts/app/app_delegate.rb
+++ ch_2/rp_alerts/app/app_delegate.rb
@@ -9,7 +9,10 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.rootViewController = navigationController
     @window.makeKeyAndVisible
-    rmq.alert('Ackbar Says', "It's a trap!", cancel_button='Cancel', other_buttons=['OK'], delegate=self)
+    alert_view = rmq.alert('Ackbar Says', "It's a trap!", cancel_button='Cancel', other_buttons=['OK'], delegate=self, UIAlertViewStyleLoginAndPasswordInput).get
+    # TODO add changing keyboard to rmq_alert gem
+    text = alert_view.textFieldAtIndex  0
+    text.keyboardType = UIKeyboardTypeNamePhonePad
 
     true
   end
